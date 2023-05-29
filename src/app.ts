87131b7938b67cdd3ee5c92c29e37f4166504b67
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2023-05-29 23:28:50 +0600

    setup eslint . prettier . husky . lint stage

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
-     app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 //Testing Route
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send({ Status: 'Success', message: 'Server is running' })
 })
 
