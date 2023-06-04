841eb31d988631ea06f96fee04ca5212a9084498
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2023-06-05 00:44:30 +0600

    [DONE] Setup Logger

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { logger, errorLogger } from './shared/logger'
 
-main().catch(err => console.log(err))
+main().catch(err => errorLogger.error(err))
 
 async function main() {
   try {
     await mongoose.connect(config.DB_URI as string)
-    console.log('DB Connected Successfully')
+    // console.log('DB Connected Successfully')
+    logger.info('DB Connected Successfully')
 
     app.listen(config.PORT, () => {
-      console.log(`App listening on port ${config.PORT}`)
+      logger.info(`App listening on port ${config.PORT}`)
     })
   } catch (err) {
-    console.log(`ERROR: Failed to Connect DB : ${err}`)
+    errorLogger.error(`ERROR: Failed to Connect DB : ${err}`)
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
---
+++ src/shared/logger.ts
@@ -0,0 +1,94 @@
+import path from 'path'
+import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+
+const { combine, timestamp, label, printf, prettyPrint } = format
+
+const myFormat = printf(({ level, message, label }) => {
+  const date = new Date()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+  return `${date.toString()} ${hour}:${minute}:${second} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'SUCCESS!' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winstone',
+        'successes',
+        '%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winstone',
+        'infos',
+        '%DATE%-info.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+const errorLogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'ERROR!' }), timestamp(), myFormat),
+  transports: [
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winstone',
+        'errors',
+        '%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winstone',
+        'infos',
+        '%DATE%-info.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+    new transports.Console(),
+  ],
+})
+
+/* if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.simple(),
+  }));
+} */
+
+export { logger, errorLogger }
